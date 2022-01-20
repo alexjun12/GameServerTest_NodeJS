d5639a8b5f158580acede9d248197fd07994c691
alexjun12 <alexjun12@naver.com>
2022-01-21 02:19:34 +0900

    game_server

--- Room.js
+++
@@ -1,9 +0,0 @@
-const roomMk = (clientNum) => {
-    if(clientNum > 3){ //client num check
-        console.log("4PlayerComplete");
-        console.log("GameStart!!");
-        return true;
-    }
-}
-
-module.exports = roomMk;
--- Server.js
+++ Server.js
@@ -1,48 +1,77 @@
-const rsv = require('./Room.js');
-
 const WebSocket = require('ws'); //get WebSocket from socket
 const sws = new WebSocket.Server({ port: 3333 },()=>{ //open port, start new server  
     console.log('StartServer!!') 
 }); 
 
-const horseInfo = {
-   level: 0,
-   speed: 0,
-   accel: 0,
-   hp: 0,
-   agility: 0,
-   consis: 0
-}
-
 let clientNum = 0; //number of connected clients 
 const clientId = []; //client Id saved
+const clientHorse = [
+   {
+      level: 0,
+      speed: 0,
+      accel: 0,
+      hp: 0,
+      agility: 0,
+      consis: 0
+   },
+   {
+      level: 0,
+      speed: 0,
+      accel: 0,
+      hp: 0,
+      agility: 0,
+      consis: 0
+   },
+   {
+      level: 0,
+      speed: 0,
+      accel: 0,
+      hp: 0,
+      agility: 0,
+      consis: 0
+   },
+   {
+      level: 0,
+      speed: 0,
+      accel: 0,
+      hp: 0,
+      agility: 0,
+      consis: 0
+   }
+]; //client horse info
+let resGStart = false;
 
 sws.on('connection', function connection(client) { //if server is connected run function
     client.on('message', (data) => {  //if server got a message from client, run function (data) is a data from client
 
       if(String(data).substring(0,2) === "id"){
-         clientNum += 1;
          clientId.push(String(data).substring(3,7));
-         console.log(`player${clientId[clientNum - 1]}Entered!!`);
+         console.log(`player${clientId[clientNum]}Entered!!`);
+         client.send(`player${clientId[clientNum]}Entered!!`);
       }
-      let resGStart = rsv(clientNum); // add clientNum to room
-      if(resGStart === true){
-        client.send("GameStart"); //request to start game
-        clientNum = 0;
-        resGStart = false;
-     }
-     
       if(String(data).substring(0,2) === "hd"){
-        horseInfo.level = Number(String(data).substring(3,4));
-        horseInfo.speed = Number(String(data).substring(4,5));
-        horseInfo.accel = Number(String(data).substring(5,6));
-        horseInfo.hp = Number(String(data).substring(6,7));
-        horseInfo.agility = Number(String(data).substring(7,8));
-        horseInfo.consis = Number(String(data).substring(8,9));
+         clientHorse[clientNum].level = Number(String(data).substring(3,4));
+         clientHorse[clientNum].speed = Number(String(data).substring(4,5));
+         clientHorse[clientNum].accel = Number(String(data).substring(5,6));
+         clientHorse[clientNum].hp = Number(String(data).substring(6,7));
+         clientHorse[clientNum].agility = Number(String(data).substring(7,8));
+         clientHorse[clientNum].consis = Number(String(data).substring(8,9));
+ 
+         clientNum += 1;
 
-        console.log(horseInfo);
+         if(clientNum > 3){
+            resGStart = true;
+         }
+         if(resGStart === true){
+           //client.send("GameStart"); //request to start game
+           clientNum = 0;
+           resGStart = false;
+           console.log(clientId);
+           console.log(clientHorse);
+           setInterval(() => client.send("hihi"), 100);
+        }
 
-        client.send(String(horseInfo.level + horseInfo.speed));
+
       }
    }) 
 }) 
